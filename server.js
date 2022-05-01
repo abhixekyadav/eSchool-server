073906c61ba8242920b9688cfe072aac1313e5db
Abhishek <idonttellanyonemyemail@gmail.com>
2022-05-01 10:47:14 +0530

    FIX: limit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose
 
 // apply middlewares
 app.use(cors());
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
